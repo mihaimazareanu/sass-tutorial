f54b62be1cae14ede4c78222d8fad0f1f7f20637
mihaimazareanu <49844133+mihaimazareanu@users.noreply.github.com>
2023-07-30 11:55:20 +0200

    initial commit

---
+++ src/components/MainPage.js
@@ -0,0 +1,12 @@
+import "../styles/MainPage.css";
+
+function MainPage() {
+  return (
+    <div className='mainPage'>
+      <h1>Welcome to my website</h1>
+      <button>Click Here</button>
+    </div>
+  )
+}
+
+export default MainPage
